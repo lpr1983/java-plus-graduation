c702a79ab1abdae7ec3c78ee37453da25a1c3af4
lpr1983 <lpr@inbox.ru>
2026-08-20 17:54:07 +0300

    1. Chekstyle. 2. Удаление неиспользуемой директории.

--- stat/stat-client/src/main/java/ewm/stat/client/config/StatClientConfig.java
+++ stat/stat-client/src/main/java/ewm/stat/client/config/StatClientConfig.java
@@ -6,9 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.backoff.FixedBackOffPolicy;
-import org.springframework.retry.policy.MaxAttemptsRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 public class StatClientConfig {
