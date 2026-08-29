33ee9b85e64887329c1cb5357573faeaafbe7a08
lpr1983 <lpr@inbox.ru>
2026-08-29 13:47:14 +0300

    Добавлен feign клиент.

---
+++ core/main-service/src/main/java/ewm/main/user/UserClient.java
@@ -0,0 +1,19 @@
+package ewm.main.user;
+
+import ewm.main.dto.UserShortDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "user-service", path = "/internal/users")
+public interface UserClient {
+
+    @GetMapping("/{userId}")
+    UserShortDto getUser(@PathVariable("userId") long userId);
+
+    @GetMapping
+    List<UserShortDto> getUsers(@RequestParam("ids") List<Long> ids);
+}
