3c87d4e4a13e2b73000316121a9a5001dc5d3649
lpr1983 <lpr@inbox.ru>
2026-09-07 16:42:19 +0300

    docker-compose.yml

---
+++ docker/init-databases.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE "ewm-stats" OWNER dbuser;
+CREATE DATABASE "ewm-users" OWNER dbuser;
+CREATE DATABASE "ewm-requests" OWNER dbuser;
+CREATE DATABASE "ewm-locations" OWNER dbuser;
+CREATE DATABASE analyzer OWNER dbuser;
